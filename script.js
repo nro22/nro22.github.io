4fafc11d5ace1faa30c6cbec76dccada2c5b4b92
nro22 <nrokita22@gmail.com>
2024-12-05 14:40:51 -0600

    nav mobile

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.querySelector('.nav-toggle');
     const navList = document.querySelector('.nav-list');
+    const navContainer = document.querySelector('.nav-container');
     
     navToggle.addEventListener('click', () => {
         navToggle.classList.toggle('active');
-        navList.classList.toggle('nav-closed');
+        navList.classList.toggle('nav-hidden');
+        navContainer.classList.toggle('collapsed');
     });
 });
