90ad6510bed920bdf6816c3f0c89abcb1b888e17
nro22 <nrokita22@gmail.com>
2024-12-27 17:16:54 -0600

    Animation nonresponsive

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
         navContainer.classList.toggle('collapsed');
     });
 });
+const scroller = document.querySelector('.scroller');
+if (!window.matchMedia( "(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+function addAnimation(){
+    scroller.setAttribute('data-animated',true);
+    const scrollerContent = scroller.querySelector('.scroller-content');
+    const scrollerItems = Array.from(scrollerContent.children);
+    scrollerItems.forEach(item =>{
+        const duplicate = item.cloneNode(true);
+        duplicate.setAttribute('aria-hidden',true);
+        scrollerContent.appendChild(duplicate);
+    })
+}
